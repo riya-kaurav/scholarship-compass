ab2fc9573cf302e94f10c04c45eff0bd81ec7b8b
riya-kaurav <kauravriya665@gmail.com>
2025-12-25 12:35:50 +0530

    scholarship-manager

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
               <GraduationCap className="h-5 w-5 text-primary-foreground" />
             </div>
             <span className="font-display font-bold text-lg text-foreground">
-              Scholar<span className="text-secondary">Hub</span>
+              Scholarship<span className="text-secondary"> Manager</span>
             </span>
           </Link>
 
